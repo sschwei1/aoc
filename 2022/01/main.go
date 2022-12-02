32833365a89c8afbb850a8bcf0a5d933290213d6
sschwei1 <24sebastian05@gmail.com>
2022-12-02 08:51:33 +0100

    cleanup day-1 code

--- 2022/01/main.go
+++ 2022/01/main.go
@@ -1,97 +1,102 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "log"
-    "bufio"
-    "strconv"
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
 )
 
 func main() {
-    result1 := solveChallengeOne()
-    fmt.Printf("Result-1: %d\n", result1)
+	result1 := solveChallengeOne()
+	fmt.Printf("Result-1: %d\n", result1)
 
-    result2 := solveChallengeTwo();
+	result2 := solveChallengeTwo()
     fmt.Printf("Result-2: %d\n", result2)
 }
 
-func solveChallengeOne() (int) {
-    elves := readElves()
-    maxVal, _ := getMaxValue(elves)
-    return maxVal
+func solveChallengeOne() int {
+	elves := readElves()
+	maxVal, _ := getMaxValue(elves)
+	return maxVal
 }
 
-func solveChallengeTwo() (int) {
-    elves := readElves()
+func solveChallengeTwo() int {
+	elves := readElves()
 
-    topThree := 0
-    for i:= 0; i < 3; i++ {
-        maxVal, index := getMaxValue(elves)
-        topThree += maxVal
-        elves = removeElementAtIndex(elves, index)
-    }
+	topThree := 0
+	for i := 0; i < 3; i++ {
+		maxVal, index := getMaxValue(elves)
+		topThree += maxVal
+		elves = removeElementAtIndex(elves, index)
+	}
 
-    return topThree
+	return topThree
 }
 
-func readElves() ([]int) {
-    // open file
-    file, err := os.Open("./input_01.txt")
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+func readElves() []int {
+	// open file
+	file, err := os.Open("./input_01.txt")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func(file *os.File) {
+		err := file.Close()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}(file)
 
-    // access file buffer
-    scanner := bufio.NewScanner(file)
+	// access file buffer
+	scanner := bufio.NewScanner(file)
 
-    // init variables
-    var elves []int
-    counter := 0
+	// init variables
+	var elves []int
+	counter := 0
 
-    // setup start
-    addElve(&elves)
+	// setup start
+	addElv(&elves)
 
-    for scanner.Scan() {
-        line := scanner.Text()
-        if len(line) == 0 {
-            counter = counter + 1
-            addElve(&elves)
-        } else {
-            calories, err := strconv.Atoi(line)
-            if err != nil {
-                log.Fatal(err)
-            }
-            elves[counter] += calories
-        }
-    }
+	for scanner.Scan() {
+		line := scanner.Text()
+		if len(line) == 0 {
+			counter = counter + 1
+			addElv(&elves)
+		} else {
+			calories, err := strconv.Atoi(line)
+			if err != nil {
+				log.Fatal(err)
+			}
+			elves[counter] += calories
+		}
+	}
 
-    return elves
+	return elves
 }
 
-func addElve(elves *[]int) {
-    *elves = append(*elves, 0)
+func addElv(elves *[]int) {
+	*elves = append(*elves, 0)
 }
 
 func getMaxValue(s []int) (int, int) {
-    if len(s) == 0 {
-        return 0, -1
-    }
+	if len(s) == 0 {
+		return 0, -1
+	}
 
-    max := s[0]
-    index := 0
+	max := s[0]
+	index := 0
 
-    for i, value := range s {
-        if value > max {
-            max = value
-            index = i
-        }
-    }
+	for i, value := range s {
+		if value > max {
+			max = value
+			index = i
+		}
+	}
 
-    return max,index
+	return max, index
 }
 
-func removeElementAtIndex(s []int, index int) ([]int) {
-    return append(s[:index], s[index+1:]...)
+func removeElementAtIndex(s []int, index int) []int {
+	return append(s[:index], s[index+1:]...)
 }
