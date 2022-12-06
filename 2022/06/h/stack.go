2863bfa0f17760c1c0975e8c7de8b9c39db37219
sschwei1 <24sebastian05@gmail.com>
2022-12-06 08:30:58 +0100

    day-6 challenge-1

---
+++ 2022/06/h/stack.go
@@ -0,0 +1,46 @@
+package h
+
+type Stack []string
+
+func (s *Stack) IsEmpty() bool {
+	return len(*s) == 0
+}
+
+func (s *Stack) Push(str string) {
+	*s = append(*s, str)
+}
+
+func (s *Stack) Pop() (string, bool) {
+	if s.IsEmpty() {
+		return "", false
+	} else {
+		index := len(*s) - 1
+		element := (*s)[index]
+		*s = (*s)[:index]
+		return element, true
+	}
+}
+
+func (s *Stack) PushFront(str string) {
+	*s = append(*s, "")
+	copy((*s)[1:], *s)
+	(*s)[0] = str
+}
+
+func (s *Stack) Peek() string {
+	return (*s)[len(*s)-1]
+}
+
+func (s *Stack) CollectionUnique() bool {
+	m := make(map[string]bool)
+
+	for _, el := range *s {
+		if m[el] {
+			return false
+		}
+
+		m[el] = true
+	}
+
+	return true
+}
